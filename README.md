21e1091b7af122fab4898a422fbd6c3b7789f0f7
Mark Egesdal <megesdal@gmail.com>
2017-07-31 15:54:49 -1000

    :tada: Initial commit (with pysdl2 hw example)

---
+++ README.md
@@ -0,0 +1,3 @@
+https://pysdl2.readthedocs.io/en/rel_0_9_5/tutorial/helloworld.html
+https://hookrace.net/blog/writing-a-2d-platform-game-in-nim-with-sdl2/
+https://www.raywenderlich.com/3611/cocos2d-tutorial-for-ios-how-to-make-a-space-shooter-iphone-game
---
+++ hw.py
@@ -0,0 +1,21 @@
+import sys
+import sdl2.ext
+
+RESOURCES = sdl2.ext.Resources(__file__, "resources")
+
+sdl2.ext.init()
+
+window = sdl2.ext.Window("Hello World!", size=(640, 480))
+window.show()
+
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+sprite = factory.from_image(RESOURCES.get_path("helloworld.jpg"))
+
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(sprite)
+
+processor = sdl2.ext.TestEventProcessor()
+processor.run(window)
+
+sdl2.ext.quit()
+
