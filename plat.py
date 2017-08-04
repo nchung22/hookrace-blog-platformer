beb009e3e193087d2e55128722d70e9e99dec519
Mark Egesdal <megesdal@gmail.com>
2017-08-04 10:52:10 -0700

    Port section 5 (physics) of hookrace platformer

--- plat.py
+++ plat.py
@@ -1,6 +1,8 @@
 import sys
 import sdl2.ext
+from math import sqrt
 from time import time
+from typing import List, Set, Tuple
 from enum import Enum, auto
 from sdl2 import SDL_FLIP_NONE, SDL_FLIP_HORIZONTAL
 from collections import namedtuple
@@ -28,6 +30,14 @@ class Point2d:
     def __add__(self, other):
         return Point2d(self.x + other.x, self.y + other.y)
 
+    def __mul__(self, scalar):
+        return Point2d(self.x * scalar, self.y * scalar)
+
+    def len(self):
+        return sqrt(self.x * self.x + self.y * self.y)
+
+Vector2d = Point2d
+
 
 class Input(Enum):
     NONE = auto()
@@ -40,27 +50,39 @@ class Input(Enum):
 
 class Player:
     texture: sdl2.ext.TextureSprite
-    pos = Point2d(0, 0)
-    vel = Point2d(0, 0)
+    pos: Point2d
+    vel: Vector2d
 
     def __init__(self, texture: sdl2.ext.TextureSprite) -> None:
         self.texture = texture
+        self.pos = None
+        self.vel = None
         self.restart()
 
     def restart(self):
         self.pos = Point2d(170, 500)
-        self.vel = Point2d(0, 0)
+        self.vel = Vector2d(0, 0)
 
+
+class Collision(Enum):
+    X = auto()
+    Y = auto()
+    CORNER = auto()
 
 TILES_PER_ROW = 16
 TILE_SIZE = Point2d(64, 64)
+PLAYER_SIZE = Point2d(64, 64)
+
+AIR = 0
+START = 78
+FINISH = 110
 
 
 class Map:
     texture: sdl2.ext.TextureSprite
     width: int
     height: int
-    tiles = []
+    tiles: List[int]
 
     def __init__(self, texture: sdl2.ext.TextureSprite, file_name: str) -> None:
         self.texture = texture
@@ -79,9 +101,70 @@ class Map:
                 width += 1
 
             if self.width > 0 and self.width != width:
-                raise "Incompatible line length in map" + file_name
+                raise RuntimeError("Incompatible line length in map " + file_name)
             self.width = width
             self.height += 1
+
+    def get_tile(self, x: int, y: int) -> int:
+        nx = min(max(int(x / TILE_SIZE.x), 0), self.width - 1)
+        ny = min(max(int(y / TILE_SIZE.y), 0), self.height - 1)
+        pos = ny * self.width + nx
+        return self.tiles[pos]
+
+    def is_tile_solid(self, x: int, y: int) -> bool:
+        return self.get_tile(x, y) not in {AIR, START, FINISH}
+
+    def is_solid(self, point: Point2d) -> bool:
+        return self.is_tile_solid(int(round(point.x)), int(round(point.y)))
+
+    def on_ground(self, pos: Point2d, size: Vector2d) -> bool:
+        size = size * 0.5
+        return (self.is_solid(Point2d(pos.x - size.x, pos.y + size.y + 1)) or
+                self.is_solid(Point2d(pos.x + size.x, pos.y + size.y + 1)))
+
+    def test_box(self, pos: Point2d, size: Vector2d) -> bool:
+        size = size * 0.5
+        return (
+            self.is_solid(Point2d(pos.x - size.x, pos.y - size.y)) or
+            self.is_solid(Point2d(pos.x + size.x, pos.y - size.y)) or
+            self.is_solid(Point2d(pos.x - size.x, pos.y + size.y)) or
+            self.is_solid(Point2d(pos.x + size.x, pos.y + size.y))
+        )
+
+    def move_box(self, pos: Point2d, vel: Vector2d, size: Vector2d) -> Tuple[Set[Collision], Point2d, Point2d]:
+        distance = vel.len()
+        maximum = int(distance)
+
+        result: Set[Collision] = set()
+        if distance < 0:
+            return result
+
+        fraction = 1.0 / float(maximum + 1)
+
+        for i in range(0, maximum + 1):
+            new_pos = pos + vel * fraction
+            if self.test_box(new_pos, size):
+                hit = False
+                if self.test_box(Point2d(pos.x, new_pos.y), size):
+                    result.add(Collision.Y)
+                    new_pos.y = pos.y
+                    vel.y = 0
+                    hit = True
+
+                if self.test_box(Point2d(new_pos.x, pos.y), size):
+                    result.add(Collision.X)
+                    new_pos.x = pos.x
+                    vel.x = 0
+                    hit = True
+
+                if not hit:
+                    result.add(Collision.CORNER)
+                    new_pos = pos
+                    vel = Vector2d(0, 0)
+
+            pos = new_pos
+
+        return result, pos, vel
 
 
 class Game:
@@ -95,7 +178,7 @@ class Game:
     }
     renderer: sdl2.ext.Renderer
     player: Player
-    camera: Point2d
+    camera: Vector2d
 
     def __init__(self, renderer: sdl2.ext.Renderer) -> None:
         self.renderer = renderer
@@ -118,8 +201,27 @@ class Game:
     def physics(self) -> None:
         if self.inputs[Input.RESTART]:
             self.player.restart()
+
+        ground = self.map.on_ground(self.player.pos, PLAYER_SIZE)
+
+        if self.inputs[Input.JUMP]:
+            if ground:
+                self.player.vel.y = -21
+
+        direction = ((1 if self.inputs[Input.RIGHT] else 0)
+                     - (1 if self.inputs[Input.LEFT] else 0))
+
         self.player.vel.y += 0.75  # gravity
-        self.player.pos += self.player.vel
+        if ground:
+            self.player.vel.x = 0.5 * self.player.vel.x + 4.0 * direction
+        else:
+            self.player.vel.x = 0.95 * self.player.vel.x + 2.0 * direction
+        self.player.vel.x = min(max(self.player.vel.x, -8), 8)
+
+        collisions, self.player.pos, self.player.vel = self.map.move_box(
+            self.player.pos, self.player.vel, PLAYER_SIZE
+        )
+        # self.player.pos += self.player.vel
 
     def render(self) -> None:
         # Draw over all drawings of the last frame with the default color
@@ -163,7 +265,7 @@ def render_tee(renderer: sdl2.ext.Renderer, texture: sdl2.ext.TextureSprite,
                       center=None, flip=part.flip)
 
 
-def render_map(renderer: sdl2.ext.Renderer, map: Map, camera: Point2d):
+def render_map(renderer: sdl2.ext.Renderer, map: Map, camera: Vector2d):
     for i, tile_nr in enumerate(map.tiles):
         if tile_nr == 0:
             continue
