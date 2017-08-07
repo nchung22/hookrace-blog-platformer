c973d8f5fdfeff805eaae1bcf88be69b8b1bac59
Mark Egesdal <megesdal@gmail.com>
2017-08-07 11:26:33 -1000

    Port section 8 and 9 (text) of hookrace platformer

--- plat.py
+++ plat.py
@@ -4,16 +4,14 @@ from math import sqrt
 from time import time
 from typing import List, Set, Tuple
 from enum import Enum, auto
-from sdl2 import SDL_FLIP_NONE, SDL_FLIP_HORIZONTAL
+from sdl2 import SDL_FLIP_NONE, SDL_FLIP_HORIZONTAL, SDL_KEYDOWN, SDL_KEYUP, SDL_QUIT, SDL_RENDERER_ACCELERATED, SDL_RENDERER_PRESENTVSYNC
+from sdl2.ext import Color, FontManager, Renderer, Resources, SpriteFactory, TextureSprite, Window
 from collections import namedtuple
 
-RESOURCES = sdl2.ext.Resources(__file__, "resources")
+Rect = namedtuple("Rect", ["x", "y", "w", "h"])
 
 
-Rect = namedtuple('Rect', ['x', 'y', 'w', 'h'])
-
-
-BodyPart = namedtuple('BodyPart', ['source', 'dest', 'flip'])
+BodyPart = namedtuple("BodyPart", ["source", "dest", "flip"])
 
 
 class Point2d:
@@ -59,13 +57,33 @@ class Time:
         self.best = -1
 
 
+class CacheLine:
+    texture: TextureSprite
+    w: int
+    h: int
+
+    def __init__(self, texture: TextureSprite, w: int, h: int) -> None:
+        self.texture = texture
+        self.w = w
+        self.h = h
+
+
+class TextCache:
+    text: str
+    cache: CacheLine
+
+    def __init__(self):
+        self.text = ""
+        self.cache = None
+
+
 class Player:
-    texture: sdl2.ext.TextureSprite
+    texture: TextureSprite
     pos: Point2d
     vel: Vector2d
     time: Time
 
-    def __init__(self, texture: sdl2.ext.TextureSprite) -> None:
+    def __init__(self, texture: TextureSprite) -> None:
         self.texture = texture
         self.time = Time()
         self.pos = None
@@ -95,12 +113,12 @@ FINISH = 110
 
 
 class Map:
-    texture: sdl2.ext.TextureSprite
+    texture: TextureSprite
     width: int
     height: int
     tiles: List[int]
 
-    def __init__(self, texture: sdl2.ext.TextureSprite, file_name: str) -> None:
+    def __init__(self, texture: TextureSprite, file_name: str) -> None:
         self.texture = texture
         self.tiles = []
         self.width = 0
@@ -195,26 +213,36 @@ class Game:
         Input.RESTART: False,
         Input.QUIT: False
     }
-    renderer: sdl2.ext.Renderer
+    renderer: Renderer
+    font: FontManager
     player: Player
+    map: Map
     camera: Vector2d
+    tc_timer: TextCache
+    tc_best_time: TextCache
 
-    def __init__(self, renderer: sdl2.ext.Renderer) -> None:
+    def __init__(self, renderer: Renderer) -> None:
         self.renderer = renderer
-        factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
-        self.player = Player(factory.from_image(RESOURCES.get_path("player.png")))
-        self.map = Map(factory.from_image(RESOURCES.get_path("grass.png")),
-                       RESOURCES.get_path("default.map"))
+
+        # load resources
+        resources = Resources(__file__, "resources")
+        self.font = FontManager(resources.get_path("DejaVuSans.ttf"), size=28)
+        factory = SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
+        self.player = Player(factory.from_image(resources.get_path("player.png")))
+        self.map = Map(factory.from_image(resources.get_path("grass.png")),
+                       resources.get_path("default.map"))
         self.camera = Point2d(0, 0)
+        self.tc_timer = TextCache()
+        self.tc_best_time = TextCache()
 
     def handle_input(self) -> None:
         events = sdl2.ext.get_events()
         for event in events:
-            if event.type == sdl2.SDL_QUIT:
+            if event.type == SDL_QUIT:
                 self.inputs[Input.QUIT] = True
-            elif event.type == sdl2.SDL_KEYDOWN:
+            elif event.type == SDL_KEYDOWN:
                 self.inputs[to_input(event.key.keysym.sym)] = True
-            elif event.type == sdl2.SDL_KEYUP:
+            elif event.type == SDL_KEYUP:
                 self.inputs[to_input(event.key.keysym.sym)] = False
 
     def physics(self) -> None:
@@ -245,11 +273,11 @@ class Game:
     def move_camera(self) -> None:
         half_win = WINDOW_SIZE.x / 2
         # 1. always in center:
-        self.camera.x = self.player.pos.x - half_win
+        # self.camera.x = self.player.pos.x - half_win
         # 2. follow once leaves center:
         left_area = self.player.pos.x - half_win - 100
         right_area = self.player.pos.x - half_win + 100
-        # self.camera.x = min(max(self.camera.x, left_area), right_area)
+        self.camera.x = min(max(self.camera.x, left_area), right_area)
         # 3. fluid
         dist = self.camera.x - self.player.pos.x + half_win
         # self.camera.x -= 0.05 * dist
@@ -265,23 +293,51 @@ class Game:
                 player_time.begin = -1
                 if player_time.best < 0 or player_time.finish < player_time.best:
                     player_time.best = player_time.finish
-                print("Finished in " + str(player_time.finish) + " ticks")
 
-    def render(self) -> None:
+    def __render_text(self, text: str, x: int, y: int, color: Color, tc: TextCache):
+        if text != tc.text:
+            # TODO: tc.cache.texture.destroy()
+            tc.cache = render_text(self.renderer, self.font, text, color)
+            tc.text = text
+
+        source = Rect(0, 0, tc.cache.w, tc.cache.h)
+        dest = Rect(x, y, tc.cache.w, tc.cache.h)
+        self.renderer.copy(tc.cache.texture, source, dest, angle=0, center=None, flip=SDL_FLIP_NONE)
+
+    def render(self, tick: int) -> None:
         # Draw over all drawings of the last frame with the default color
         self.renderer.clear()
         # Actual drawing here
         render_tee(
-            self.renderer,
-            self.player.texture,
-            self.player.pos - self.camera
-        )
+            self.renderer, self.player.texture,
+            self.player.pos - self.camera)
         render_map(self.renderer, self.map, self.camera)
+
+        player_time = self.player.time
+        white = Color(255, 255, 255)
+        if player_time.begin >= 0:
+            self.__render_text(format_time_exact(tick - player_time.begin), 50, 100, white, self.tc_timer)
+        elif player_time.finish >= 0:
+            self.__render_text("Finished in: " + format_time_exact(player_time.finish), 50, 100, white, self.tc_timer)
+        if player_time.best >= 0:
+            self.__render_text("Best time: " + format_time_exact(player_time.best), 50, 150, white, self.tc_best_time)
+
         # Show the result on screen
         self.renderer.present()
 
 
-def render_tee(renderer: sdl2.ext.Renderer, texture: sdl2.ext.TextureSprite,
+def format_time(ticks: int) -> str:
+    mins = int(int(ticks / 50) / 60)
+    secs = int(ticks / 50) % 60
+    return f"{mins:02}:{secs:02}"
+
+
+def format_time_exact(ticks: int) -> str:
+    cents = (ticks % 50) * 2
+    return f"{format_time(ticks)}:{cents:02}"
+
+
+def render_tee(renderer: Renderer, texture: TextureSprite,
                pos: Point2d):
     x = int(pos.x)
     y = int(pos.y)
@@ -309,7 +365,7 @@ def render_tee(renderer: sdl2.ext.Renderer, texture: sdl2.ext.TextureSprite,
                       center=None, flip=part.flip)
 
 
-def render_map(renderer: sdl2.ext.Renderer, map: Map, camera: Vector2d):
+def render_map(renderer: Renderer, map: Map, camera: Vector2d):
     for i, tile_nr in enumerate(map.tiles):
         if tile_nr == 0:
             continue
@@ -321,6 +377,18 @@ def render_map(renderer: sdl2.ext.Renderer, map: Map, camera: Vector2d):
         clip = Rect(clip_x, clip_y, TILE_SIZE.x, TILE_SIZE.y)
         dest = Rect(dest_x, dest_y, TILE_SIZE.x, TILE_SIZE.y)
         renderer.copy(map.texture, clip, dest)
+
+
+def render_text(
+        renderer: Renderer, font: FontManager,
+        text: str, color: Color) -> CacheLine:
+    surface = font.render(text, color=color)
+    # TODO: surface.setSurfaceAlphaMod
+
+    factory = SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
+    texture = factory.from_surface(surface, free=True)
+
+    return CacheLine(texture, surface.w, surface.h)
 
 
 def to_input(key):
@@ -341,16 +409,16 @@ def to_input(key):
 def main() -> int:
     sdl2.ext.init()
 
-    window = sdl2.ext.Window("Our own 2D platformer", size=(WINDOW_SIZE.x, WINDOW_SIZE.y))
+    window = Window("Our own 2D platformer", size=(WINDOW_SIZE.x, WINDOW_SIZE.y))
     window.show()
 
-    renderer = sdl2.ext.Renderer(
+    renderer = Renderer(
         window,
         index=-1,
-        flags=sdl2.SDL_RENDERER_ACCELERATED | sdl2.SDL_RENDERER_PRESENTVSYNC
+        flags=SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     )
 
-    renderer.color = sdl2.ext.Color(r=110, g=132, b=174)
+    renderer.color = Color(r=110, g=132, b=174)
 
     game = Game(renderer)
 
@@ -361,13 +429,13 @@ def main() -> int:
         game.handle_input()
 
         new_tick = int((time() - start_time) * 50)
-        for tick in range(last_tick, new_tick):
+        for tick in range(last_tick + 1, new_tick + 1):
             game.physics()
             game.move_camera()
             game.logic(tick)
         last_tick = new_tick
 
-        game.render()
+        game.render(last_tick)
 
     sdl2.ext.quit()
     return 0
